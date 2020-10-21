d3ad319dfe607f543868d21c6c655b9f32a96822
aweikalee <aweikalee@163.com>
2020-10-21 18:32:53 +0800

    fix: viewport尺寸强制为偶数，避免因奇数出现的绘图异常

--- src/store/viewport.ts
+++ src/store/viewport.ts
@@ -1,13 +1,13 @@
 import { reactive, readonly } from 'vue'
 
 const state = reactive({
-  w: window.innerWidth,
-  h: window.innerHeight,
+  w: (window.innerWidth >> 1) << 1,
+  h: (window.innerHeight >> 1) << 1,
 })
 
 export const viewport = readonly(state)
 
 addEventListener('resize', () => {
-  state.w = window.innerWidth
-  state.h = window.innerHeight
+  state.w = (window.innerWidth >> 1) << 1
+  state.h = (window.innerHeight >> 1) << 1
 })
