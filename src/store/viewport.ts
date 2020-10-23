069f948cc66317d11ae029b9e75e257f00ef6854
aweikalee <aweikalee@163.com>
2020-10-23 22:48:50 +0800

    feat: viewport 增加 orientation 判断横竖屏

--- src/store/viewport.ts
+++ src/store/viewport.ts
@@ -1,13 +1,20 @@
 import { reactive, readonly } from 'vue'
 
+export type IOrientation = 'portrait' | 'landscape'
+
 const state = reactive({
   w: (window.innerWidth >> 1) << 1,
   h: (window.innerHeight >> 1) << 1,
+  orientation:
+    window.innerHeight >= window.innerWidth ? 'portrait' : 'landscape',
 })
 
 export const viewport = readonly(state)
 
 addEventListener('resize', () => {
-  state.w = (window.innerWidth >> 1) << 1
-  state.h = (window.innerHeight >> 1) << 1
+  const w = window.innerWidth
+  const h = window.innerHeight
+  state.w = (w >> 1) << 1
+  state.h = (h >> 1) << 1
+  state.orientation = h >= w ? 'portrait' : 'landscape'
 })
