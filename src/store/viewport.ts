b832ba02af626a2cf2029fb39c0026f250b57a13
aweikalee <aweikalee@163.com>
2020-10-23 22:56:26 +0800

    refactor: viewport 初始化 和 resize 的计算 合并到 init 函数中

--- src/store/viewport.ts
+++ src/store/viewport.ts
@@ -3,18 +3,19 @@ import { reactive, readonly } from 'vue'
 export type IOrientation = 'portrait' | 'landscape'
 
 const state = reactive({
-  w: (window.innerWidth >> 1) << 1,
-  h: (window.innerHeight >> 1) << 1,
-  orientation:
-    window.innerHeight >= window.innerWidth ? 'portrait' : 'landscape',
+  w: 0,
+  h: 0,
+  orientation: 'portrait' as IOrientation,
 })
 
 export const viewport = readonly(state)
 
-addEventListener('resize', () => {
+const init = () => {
   const w = window.innerWidth
   const h = window.innerHeight
   state.w = (w >> 1) << 1
   state.h = (h >> 1) << 1
   state.orientation = h >= w ? 'portrait' : 'landscape'
-})
+}
+init()
+addEventListener('resize', init)
