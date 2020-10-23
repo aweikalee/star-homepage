b8aad13dfcc3ad16c190203b32c109a2674a33aa
aweikalee <aweikalee@163.com>
2020-10-23 18:55:11 +0800

    refactor: 将 TheScreen 关于 canvas 的内容移至独立的 TheConstellation

---
+++ src/components/TheScreen/TheConstellation.tsx
@@ -0,0 +1,56 @@
+import { defineComponent, onMounted, ref, watch } from 'vue'
+import { Renderer, Transform, Camera } from 'ogl-typescript'
+import { glstate, viewport } from '../../store'
+import { useRaf } from '../../utils'
+import { createScene } from './scene/createScene'
+
+import styles from './styles.module.scss'
+
+export default defineComponent({
+  name: 'TheConstellation',
+  setup() {
+    const el = ref<HTMLCanvasElement>()
+
+    let renderer: Renderer | undefined
+    let scene: Transform | undefined
+    let camera: Camera | undefined
+
+    /* 重置尺寸 */
+    const onResize = () => {
+      const { w, h } = viewport
+      renderer?.setSize(w, h)
+      camera?.perspective({
+        fov: glstate.fov,
+        aspect: w / h,
+      })
+    }
+    watch(viewport, onResize)
+
+    /* 初始化 */
+    const initCanvas = () => {
+      const canvas = el.value!
+
+      renderer = new Renderer({ canvas, alpha: true })
+      const { gl } = renderer
+
+      camera = new Camera(gl)
+
+      scene = createScene(gl)
+
+      onResize()
+    }
+    onMounted(initCanvas)
+
+    /* 动画 */
+    useRaf(() => {
+      const { x, y, z } = glstate.transform.position
+      camera?.lookAt([x, -y, z])
+      renderer?.render({
+        scene,
+        camera,
+      })
+    })
+
+    return () => <canvas class={styles.constellation} ref={el}></canvas>
+  },
+})
--- src/components/TheScreen/TheScreen.tsx
+++ src/components/TheScreen/TheScreen.tsx
@@ -1,56 +1,13 @@
-import { defineComponent, onMounted, ref, watch } from 'vue'
-import { Renderer, Transform, Camera } from 'ogl-typescript'
-import { glstate, viewport } from '../../store'
-import { useRaf } from '../../utils'
-import { createScene } from './scene/createScene'
-
-import styles from './styles.module.scss'
+import { defineComponent } from 'vue'
+import TheConstellation from './TheConstellation'
 
 export default defineComponent({
   name: 'TheScreen',
   setup() {
-    const el = ref<HTMLCanvasElement>()
-
-    let renderer: Renderer | undefined
-    let scene: Transform | undefined
-    let camera: Camera | undefined
-
-    /* 重置尺寸 */
-    const onResize = () => {
-      const { w, h } = viewport
-      renderer?.setSize(w, h)
-      camera?.perspective({
-        fov: glstate.fov,
-        aspect: w / h,
-      })
-    }
-    watch(viewport, onResize)
-
-    /* 初始化 */
-    const initCanvas = () => {
-      const canvas = el.value!
-
-      renderer = new Renderer({ canvas, alpha: true })
-      const { gl } = renderer
-
-      camera = new Camera(gl)
-
-      scene = createScene(gl)
-
-      onResize()
-    }
-    onMounted(initCanvas)
-
-    /* 动画 */
-    useRaf(() => {
-      const { x, y, z } = glstate.transform.position
-      camera?.lookAt([x, -y, z])
-      renderer?.render({
-        scene,
-        camera,
-      })
-    })
-
-    return () => <canvas class={styles.screen} ref={el}></canvas>
+    return () => (
+      <>
+        <TheConstellation />
+      </>
+    )
   },
 })
