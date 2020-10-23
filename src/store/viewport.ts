e7e0631c351f075ce134bbe837e0df94ded23916
aweikalee <aweikalee@163.com>
2020-10-23 23:08:07 +0800

    feat: viewport 增加 isMobileInCSS

--- src/store/viewport.ts
+++ src/store/viewport.ts
@@ -1,4 +1,5 @@
 import { reactive, readonly } from 'vue'
+import { variable } from '../config'
 
 export type IOrientation = 'portrait' | 'landscape'
 
@@ -6,16 +7,19 @@ const state = reactive({
   w: 0,
   h: 0,
   orientation: 'portrait' as IOrientation,
+  isMobileInCSS: false,
 })
 
 export const viewport = readonly(state)
 
+const { w: boundaryW, h: boundaryH } = variable.screenBoundary
 const init = () => {
   const w = window.innerWidth
   const h = window.innerHeight
   state.w = (w >> 1) << 1
   state.h = (h >> 1) << 1
   state.orientation = h >= w ? 'portrait' : 'landscape'
+  state.isMobileInCSS = w < boundaryW || h < boundaryH
 }
 init()
 addEventListener('resize', init)
