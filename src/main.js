269aa21778b5e278868043beef1c00ad2cd3bd15
aweikalee <aweikalee@163.com>
2020-11-05 19:56:10 +0800

    fix: Safari 视口高度的显示

--- src/main.js
+++ src/main.js
@@ -2,5 +2,16 @@ import { createApp } from 'vue'
 import 'normalize.css'
 import './index.scss'
 import App from './App'
+import { throttle } from './utils'
 
-createApp(App).mount('#app')
+const app = document.getElementById('app')
+
+createApp(App).mount(app)
+
+const resize = () => {
+  app.style.width = `${window.innerWidth}px`
+  app.style.height = `${window.innerHeight}px`
+  window.scrollTo({ top: 0 })
+}
+resize()
+addEventListener('resize', throttle(resize, 16))
