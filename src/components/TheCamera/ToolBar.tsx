b7f89e21eef81b10756ebf51d52a72dd7ac71985
aweikalee <aweikalee@163.com>
2020-11-17 10:42:16 +0800

    refactor: 优化 ToolBar

--- src/components/TheCamera/ToolBar.tsx
+++ src/components/TheCamera/ToolBar.tsx
@@ -1,61 +1,89 @@
-import { defineComponent } from 'vue'
+import { defineComponent, PropType } from 'vue'
 import { camera } from '../../store'
 import { preventOrbit } from '../../webgl/utils'
 import Icon from '../Icon'
 
 import styles from './styles.module.scss'
 
+const Item = defineComponent({
+  name: 'TheCameraToolBarItem',
+  props: {
+    active: {
+      type: Boolean,
+      default: false,
+    },
+    onClick: {
+      type: Function as PropType<(e: Event) => void>,
+    },
+  },
+  setup(props, { slots }) {
+    return () => (
+      <div class={styles.item}>
+        <div
+          class={styles.button}
+          data-active={props.active}
+          {...preventOrbit}
+          onClick={(e) => {
+            preventOrbit.onClick(e)
+            props.onClick?.(e)
+          }}
+        >
+          {slots.default?.()}
+        </div>
+      </div>
+    )
+  },
+})
+
 export default defineComponent({
   name: 'TheCameraToolBar',
   setup() {
     return () => (
       <div class={styles.toolbar}>
-        <div
-          class={styles.toolbar__button}
-          data-active={camera.visible.constellation}
-          {...preventOrbit}
-          onClick={(e) => {
-            preventOrbit.onClick(e)
+        {/* 星座 */}
+        <Item
+          active={camera.visible.constellation}
+          onClick={() => {
             camera.toggleVisible('constellation')
           }}
         >
-          <Icon value="constellation" />
-        </div>
+          {() => <Icon value="constellation" />}
+        </Item>
 
-        <div
-          class={styles.toolbar__button}
-          data-active={!!camera.delay}
-          {...preventOrbit}
-          onClick={(e) => {
-            preventOrbit.onClick(e)
+        {/* 延时 */}
+        <Item
+          active={!!camera.delay}
+          onClick={() => {
             camera.toggleDelay()
           }}
         >
-          <Icon value="timer" />
-          {camera.delay ? <span style={{fontSize: '0.5em'}}>{camera.delay / 1000}秒</span> : null}
-        </div>
+          {() => (
+            <>
+              <Icon value="timer" />
+              {camera.delay ? (
+                <span class={styles.description}>{camera.delay / 1000}秒</span>
+              ) : null}
+            </>
+          )}
+        </Item>
 
-        <div
-          class={styles.toolbar__button}
-          {...preventOrbit}
-          onClick={(e) => {
-            preventOrbit.onClick(e)
+        {/* 色彩平衡 */}
+        <Item
+          onClick={() => {
             camera.toggleVisible('balance')
           }}
         >
-          <Icon value="balance" />
-        </div>
+          {() => <Icon value="balance" />}
+        </Item>
 
-        <div
-          class={styles.toolbar__button}
-          {...preventOrbit}
-          onClick={(e) => {
-            preventOrbit.onClick(e)
+        {/* 图鉴 */}
+        <Item
+          onClick={() => {
             camera.toggleVisible('list')
           }}
         >
-          <Icon value="list" />
-        </div>
+          {() => <Icon value="list" />}
+        </Item>
       </div>
     )
   },
