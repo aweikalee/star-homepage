4ec09c44afe3400ac31766c021efffbfc927e684
aweikalee <aweikalee@163.com>
2021-03-01 13:29:16 +0800

    chore: vite 升级至 2.0.4

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { UserConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 import vitePluginString from 'vite-plugin-string'
 
 export default <UserConfig>{
   esbuildTarget: 'es6',
-  plugins: [vitePluginString()],
+  plugins: [vue(), vueJsx(), vitePluginString()],
   optimizeDeps: {
     include: ['photoswipe/dist/photoswipe-ui-default'],
   },
