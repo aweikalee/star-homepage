715ded59581153ba35c9120d8b64f2bbcf1e17b6
aweikalee <aweikalee@163.com>
2021-03-06 14:44:19 +0800

    fix: 根DOM 与 store.viewport 状态不同步的问题

--- src/main.js
+++ src/main.js
@@ -2,17 +2,6 @@ import { createApp } from 'vue'
 import 'normalize.css'
 import './index.scss'
 import App from './App'
-import { throttle } from './utils'
+import { viewport } from './store'
 
-const app = document.getElementById('app')
-
-createApp(App).mount(app)
-
-const scrollToTop = throttle(() => window.scrollTo({ top: 0 }), 16)
-const resize = () => {
-  app.style.width = `${window.innerWidth}px`
-  app.style.height = `${window.innerHeight}px`
-  scrollToTop()
-}
-resize()
-addEventListener('resize', resize, { capture: true })
+createApp(App).mount(viewport.root)
--- src/store/viewport.ts
+++ src/store/viewport.ts
@@ -5,8 +5,9 @@ import { throttle } from '../utils'
 export type IOrientation = 'portrait' | 'landscape'
 
 const state = reactive({
-  w: 0,
-  h: 0,
+  root: document.getElementById('app')!,
+  w: 1,
+  h: 1,
   orientation: 'portrait' as IOrientation,
   isMobileInCSS: false,
 })
@@ -14,13 +15,18 @@ const state = reactive({
 export const viewport = readonly(state)
 
 const { w: boundaryW, h: boundaryH } = variable.screenBoundary
-const init = () => {
+const resize = () => {
   const w = window.innerWidth
   const h = window.innerHeight
+
+  state.root.style.width = `${w}px`
+  state.root.style.height = `${h}px`
+  window.scrollTo({ top: 0 })
+
   state.w = (w >> 1) << 1
   state.h = (h >> 1) << 1
   state.orientation = h >= w ? 'portrait' : 'landscape'
   state.isMobileInCSS = w < boundaryW || h < boundaryH
 }
-init()
-addEventListener('resize', throttle(init, 16))
+resize()
+addEventListener('resize', throttle(resize))
